9160a56c05230f0a8c0a21a60b8ee371741efc82
matiasdaza <matiasdazaa@gmail.com>
2017-12-19 14:30:01 -0300

    Add: Categories and show all categories

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,7 +1,9 @@
 class ApplicationController < ActionController::Base
+
   protect_from_forgery with: :exception
 
   before_action :configure_permitted_parameters, if: :devise_controller?
+  before_action :set_categoires
 
   protected
 
@@ -10,6 +12,11 @@ class ApplicationController < ActionController::Base
       devise_parameter_sanitizer.permit(:sign_up, keys: [:name, :email, :password])
       devise_parameter_sanitizer.permit(:sign_in, keys: [:name, :email, :password])
       devise_parameter_sanitizer.permit(:account_update, keys: [:name, :email, :password, :current_password])
+  end
+
+  #Como todos los controladores heredan de ApplicationController, podemos compartir aquí a categories
+  def set_categoires
+    @categories = Category.all
   end
 
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20171218180832) do
+ActiveRecord::Schema.define(version: 20171219162407) do
 
   create_table "articles", force: :cascade do |t|
     t.string "title"
@@ -24,6 +24,13 @@ ActiveRecord::Schema.define(version: 20171218180832) do
     t.integer "cover_file_size"
     t.datetime "cover_updated_at"
     t.index ["user_id"], name: "index_articles_on_user_id"
+  end
+
+  create_table "categories", force: :cascade do |t|
+    t.string "name"
+    t.string "color"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
   create_table "comments", force: :cascade do |t|
